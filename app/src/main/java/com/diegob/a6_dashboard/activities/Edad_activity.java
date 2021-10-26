f98e79aa2d5f5f7844c253a1940dd9ec2010d07f
dbeag <dbeag@iesch.org>
2021-10-26 16:36:19 +0200

    Edad canina

--- app/src/main/java/com/diegob/a6_dashboard/activities/Edad_activity.java
+++ app/src/main/java/com/diegob/a6_dashboard/activities/Edad_activity.java
@@ -3,6 +3,11 @@ package com.diegob.a6_dashboard.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.diegob.a6_dashboard.R;
 
@@ -12,5 +17,26 @@ public class Edad_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edad);
+
+        EditText edadEdit = findViewById(R.id.edad_et);
+        TextView resultadoText = findViewById(R.id.resultado_tv);
+        Button calcularBtn = findViewById(R.id.calcular_btn);
+        Log.d("Edad_Activity", "La actividad principal se ha creado");
+
+        calcularBtn.setOnClickListener(view -> {
+            String edad = edadEdit.getText().toString();
+            if (!edad.isEmpty()) {
+                int edadint = Integer.parseInt(edad);
+
+                int resultado = edadint * 7;
+
+                String resultadoFinal = getString(R.string.app_textResult, resultado);
+
+                resultadoText.setText(resultadoFinal);
+            } else{
+                Log.v("MainActivity", "Has introducido un campo vacío");
+                Toast.makeText(this, getString(R.string.app_toast), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
